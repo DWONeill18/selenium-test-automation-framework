d89a30583d09de28a286c8ad8bd1ca34da7aa507
DWONeill18 <d.w.oneill.18@gmail.com>
2026-05-16 00:03:55 +0100

    Added LoginPage with private fields and public methods to set username and password for logging in.

---
+++ selenium-tests/src/main/java/com/example/base/LoginPage.java
@@ -0,0 +1,35 @@
+package com.example.base;
+
+import org.openqa.selenium.By;
+
+public class LoginPage extends BasePage {
+
+    private By usernameField  = By.cssSelector("[data-testid='username']");
+    private By passwordField = By.cssSelector("[data-testid='password']");
+    private By loginButton = By.cssSelector("[data-testid='login-btn']");
+    private By errorMessage = By.cssSelector("[data-testid='login-error']");
+
+    public void setUsername(String username) {
+        set(usernameField, username);
+    }
+
+    public void setPassword(String password) {
+        set(passwordField, password);
+    }
+
+    public FormPage clickLoginButton() {
+        scroll(loginButton);
+        click(loginButton);
+        return new FormPage();
+    }
+
+    public FormPage login(String username, String password) {
+        setUsername(username);
+        setPassword(password);
+        return clickLoginButton();
+    }
+
+    public String getErrorMessage() {
+        return find(errorMessage).getText();
+    }
+}
