63f3c84b082362cb5d0746f6cf7b7a7fd62d79fc
DWONeill18 <d.w.oneill.18@gmail.com>
2026-05-17 23:10:27 +0100

    Updated LoginTest with tester login and failing login cases

--- selenium-tests/src/test/java/com/example/tests/LoginTest.java
+++ selenium-tests/src/test/java/com/example/tests/LoginTest.java
@@ -1,18 +1,35 @@
 package com.example.tests;
 
 import com.example.base.BaseTest;
-import com.example.base.FormPage;
-import com.example.base.LoginPage;
+import com.example.pages.FormPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
     @Test
-    public void loginTest(){
+    public void adminLoginTest(){
         FormPage formPage = loginPage.login("admin", "admin123");
         String actualText = formPage.getFormHeaderText();
         String expectedText = "Form";
         Assert.assertEquals(actualText, expectedText);
     }
+
+    @Test
+    public void testerLoginTest(){
+        FormPage formPage = loginPage.login("tester", "test123");
+        String actualText = formPage.getFormHeaderText();
+        String expectedText = "Form";
+        Assert.assertEquals(actualText, expectedText);
+    }
+
+    @Test
+    public void failingLoginTest(){
+        loginPage.login("abc", "abc123");
+        String actualText = loginPage.getErrorMessage();
+        String expectedText = "Login failed";
+        Assert.assertEquals(actualText, expectedText);
+    }
+
+
 }
