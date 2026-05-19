293961001faf114e9765eb09ad2d6f40e0861b95
DWONeill18 <d.w.oneill.18@gmail.com>
2026-05-19 23:08:08 +0100

    Added DropDownUtility class with methods to find a dropdown and select by different attributes.

---
+++ selenium-tests/src/main/java/com/example/utils/DropDownUtility.java
@@ -0,0 +1,21 @@
+package com.example.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDownUtility extends Utility{
+
+    private static Select findDropDown(By locator) { return new Select(driver.findElement(locator)); }
+
+    public static void selectByVisibleText(By locator, String text) {
+        findDropDown(locator).selectByVisibleText(text);
+    }
+
+    public static void selectByValue(By locator, String value) {
+        findDropDown(locator).selectByValue(value);
+    }
+
+    public static void selectByIndex(int index, By locator) {
+        findDropDown(locator).selectByIndex(index);
+    }
+}
