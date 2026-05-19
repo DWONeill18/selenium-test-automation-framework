8487ed9b582f348346e1cbc93566917fcc33b243
DWONeill18 <d.w.oneill.18@gmail.com>
2026-05-20 00:26:05 +0100

    Updated DropDownUtility with methods to get the selected option and to check if a specific option is selected.

--- selenium-tests/src/main/java/com/example/utils/DropDownUtility.java
+++ selenium-tests/src/main/java/com/example/utils/DropDownUtility.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.support.ui.Select;
 
 public class DropDownUtility extends Utility{
 
-    private static Select findDropDown(By locator) { return new Select(driver.findElement(locator)); }
+        private static Select findDropDown(By locator) { return new Select(driver.findElement(locator)); }
 
-    public static void selectByVisibleText(By locator, String text) {
-        findDropDown(locator).selectByVisibleText(text);
-    }
+        public static void selectByVisibleText(By locator, String text) {
+            findDropDown(locator).selectByVisibleText(text);
+        }
 
     public static void selectByValue(By locator, String value) {
         findDropDown(locator).selectByValue(value);
@@ -17,5 +17,13 @@ public class DropDownUtility extends Utility{
 
     public static void selectByIndex(int index, By locator) {
         findDropDown(locator).selectByIndex(index);
+    }
+
+    public static String getSelectedOption(By locator) {
+            return findDropDown(locator).getFirstSelectedOption().getText();
+    }
+
+    public static boolean isOptionSelected(By locator, String expectedOption) {
+            return getSelectedOption(locator).equals(expectedOption);
     }
 }
