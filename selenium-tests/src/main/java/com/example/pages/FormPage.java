ddff51f68e8a86a155eac91aa70227e53e676715
DWONeill18 <d.w.oneill.18@gmail.com>
2026-05-22 23:17:19 +0100

    Updated FormPage with click methods, is selected methods and get text methods.

--- selenium-tests/src/main/java/com/example/pages/FormPage.java
+++ selenium-tests/src/main/java/com/example/pages/FormPage.java
@@ -1,80 +1,162 @@
-package com.example.base;
+package com.example.pages;
 
+import com.example.base.BasePage;
 import org.openqa.selenium.By;
 
+import static com.example.utils.DropDownUtility.selectByVisibleText;
 import static com.example.utils.GetUtility.getText;
 
 public class FormPage extends BasePage {
 
     private By formHeader = By.xpath("//*[@id=\"root\"]/div/div/h2");
 
-    private By femaleRadioButton = By.id("gender-radio-2");
-    private By maleRadioButton = By.id("gender-radio-2");
+    private By browserAlertCheckBox = By.cssSelector("[data-testid='toggle-alert']");
 
-    private By sportsHobbyCheckBox = By.id("hobbies-checkbox-1");
-    private By readingHobbyCheckBox = By.id("hobbies-checkbox-2");
-    private By musicHobbyCheckBox = By.id("hobbies-checkbox-3");
+    private By lowPriorityRadioButton = By.xpath("//input[@type='radio']/following-sibling::text()[contains(.,'low')]");
+    private By mediumPriorityRadioButton = By.xpath("//label[contains(.,'medium')]//input[@type='radio']");
+    private By highPriorityRadioButton = By.xpath("//label[contains(.,'high')]//input[@type='radio']");
 
-    private By submitButton = By.id("submit");
-    private By bannerCheckBox = By.id("banner-checkbox-1");
+    private By sportsCheckBox = By.xpath("//label[contains(.,'Sports')]//input[@type='checkbox']");
+    private By codingCheckBox = By.xpath("//label[contains(.,'Coding')]//input[@type='checkbox']");
+    private By musicCheckBox = By.xpath("//label[contains(.,'Music')]//input[@type='checkbox']");
+
+    private By countryDropdown = By.xpath("//h4[contains(.,'Country')]/following::select[1]");
+
+    private By submitButton = By.xpath("//button[text()='Submit']");
+
+    private By formSubmitionText = By.xpath("//div[@data-testid='form-success']");
+    private By submitionAlert = By.id("form-submition-alert-1");
 
 
     public String getFormHeaderText() {
         return getText(formHeader);
     }
 
-    public void clickFemaleRadioButton() {
-        scroll(femaleRadioButton);
-        click(femaleRadioButton);
-    }
-
-    public void clickMaleRadioButton() {
-        scroll(maleRadioButton);
-        click(maleRadioButton);
-    }
-
-    public boolean isFemaleRadioButtonSelected() {
-        return find(femaleRadioButton).isSelected();
-    }
-
-    public boolean isMaleRadioButtonSelected() {
-        return find(maleRadioButton).isSelected();
-    }
-
-    public void clickSportsHobbyCheckBox() {
-        if (!find(sportsHobbyCheckBox).isSelected()) {
-        waitForElementToBeClickable(sportsHobbyCheckBox, 5);
-        scroll(sportsHobbyCheckBox);
-        click(sportsHobbyCheckBox);
+    public void clickBrowserAlertCheckBox() {
+        if (!find(browserAlertCheckBox).isSelected()) {
+            waitForElementToBeClickable(browserAlertCheckBox, 5);
+            scroll(browserAlertCheckBox);
+            click(browserAlertCheckBox);
         }
     }
 
-    public void clickReadingHobbyCheckBox() {
-        if (!find(readingHobbyCheckBox).isSelected()) {
-            waitForElementToBeClickable(readingHobbyCheckBox, 5);
-            scroll(readingHobbyCheckBox);
-            click(readingHobbyCheckBox);
+    public void unclickBrowserAlertCheckBox() {
+        if (find(browserAlertCheckBox).isSelected()) {
+            waitForElementToBeClickable(browserAlertCheckBox, 5);
+            scroll(browserAlertCheckBox);
+            click(browserAlertCheckBox);
         }
     }
 
-    public void clickMusicHobbyCheckBox() {
-        if (!find(musicHobbyCheckBox).isSelected()) {
-            waitForElementToBeClickable(musicHobbyCheckBox, 5);
-            scroll(musicHobbyCheckBox);
-            click(musicHobbyCheckBox);
+    public String getSubmitionText() {
+        return getText(formSubmitionText);
+    }
+
+    public boolean isBrowserAlertCheckBoxSelected() {
+        return find(browserAlertCheckBox).isSelected();
+    }
+
+    public void clickLowPriorityRadioButton() {
+        scroll(lowPriorityRadioButton);
+        click(lowPriorityRadioButton);
+    }
+
+    public void clickMediumPriorityRadioButton() {
+        scroll(mediumPriorityRadioButton);
+        click(mediumPriorityRadioButton);
+    }
+
+    public void clickHighPriorityRadioButton() {
+        scroll(highPriorityRadioButton);
+        click(highPriorityRadioButton);
+    }
+
+    public boolean isLowPriorityButtonSelected() {
+        return find(lowPriorityRadioButton).isSelected();
+    }
+
+    public boolean isMediumPriorityRadioButtonSelected() {
+        return find(mediumPriorityRadioButton).isSelected();
+    }
+
+    public boolean isHighPriorityRadioButtonSelected() {
+        return find(highPriorityRadioButton).isSelected();
+    }
+
+    public void clickSportCheckBox() {
+        if (!find(sportsCheckBox).isSelected()) {
+        waitForElementToBeClickable(sportsCheckBox, 5);
+        scroll(sportsCheckBox);
+        click(sportsCheckBox);
         }
+    }
+
+    public void unclickSportCheckBox() {
+        if (find(sportsCheckBox).isSelected()) {
+            waitForElementToBeClickable(sportsCheckBox, 5);
+            scroll(sportsCheckBox);
+            click(sportsCheckBox);
+        }
+    }
+
+    public void clickCodingCheckBox() {
+        if (!find(codingCheckBox).isSelected()) {
+            waitForElementToBeClickable(codingCheckBox, 5);
+            scroll(codingCheckBox);
+            click(codingCheckBox);
+        }
+    }
+
+    public void unclickCodingCheckBox() {
+        if (find(codingCheckBox).isSelected()) {
+            waitForElementToBeClickable(codingCheckBox, 5);
+            scroll(codingCheckBox);
+            click(codingCheckBox);
+        }
+    }
+
+    public void clickMusicCheckBox() {
+        if (!find(musicCheckBox).isSelected()) {
+            waitForElementToBeClickable(musicCheckBox, 5);
+            scroll(musicCheckBox);
+            click(musicCheckBox);
+        }
+    }
+
+    public void unclickMusicCheckBox() {
+        if (find(musicCheckBox).isSelected()) {
+            waitForElementToBeClickable(musicCheckBox, 5);
+            scroll(musicCheckBox);
+            click(musicCheckBox);
+        }
+    }
+
+    public boolean isSportSelected() {
+        return find(sportsCheckBox).isSelected();
+    }
+
+    public boolean isCodingSelected() {
+        return find(codingCheckBox).isSelected();
+    }
+
+    public boolean isMusicSelected() {
+        return find(musicCheckBox).isSelected();
+    }
+
+    public void selectUK(By locator, String text) {
+        selectByVisibleText(countryDropdown, "UK");
+    }
+
+    public void selectUS(By locator, String text) {
+        selectByVisibleText(countryDropdown, "USA");
+    }
+
+    public void selectIndia(By locator, String text) {
+        selectByVisibleText(countryDropdown, "India");
     }
 
     public void clickSubmitButton() {
         scroll(submitButton);
         click(submitButton);
-    }
-
-    public void clickBannerCheckBox() {
-        if (!find(bannerCheckBox).isSelected()) {
-            waitForElementToBeClickable(bannerCheckBox, 5);
-            scroll(bannerCheckBox);
-            click(bannerCheckBox);
-        }
     }
 }
