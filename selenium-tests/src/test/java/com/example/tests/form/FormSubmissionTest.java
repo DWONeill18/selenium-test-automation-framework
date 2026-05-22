c16e1ab008e4f0600f4e21777de4b3fcb29ea47f
DWONeill18 <d.w.oneill.18@gmail.com>
2026-05-22 23:19:49 +0100

    Added formSubmissionTest that unclicks the browser alert checkbox and submits the form.

---
+++ selenium-tests/src/test/java/com/example/tests/form/FormSubmissionTest.java
@@ -0,0 +1,19 @@
+package com.example.tests.form;
+
+import com.example.base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FormSubmissionTest extends BaseTest {
+
+    @Test
+    public void formSubmissionTest() {
+        var  formPage = loginPage.login("admin", "admin123");
+        formPage.unclickBrowserAlertCheckBox();
+        formPage.clickSubmitButton();
+
+        String submissionText = formPage.getSubmitionText();
+        String actualSubmissionText = "Form submitted successfully";
+        Assert.assertEquals(submissionText, actualSubmissionText, "\n Form submission text is incorrect \n");
+    }
+}
