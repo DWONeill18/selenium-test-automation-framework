db944bed3141711d0a6271b83dd631761bed622a
DWONeill18 <d.w.oneill.18@gmail.com>
2026-06-05 17:04:48 +0100

    Updated AdminPage with click page button methods and fields.

--- selenium-tests/src/main/java/com/example/pages/AdminPage.java
+++ selenium-tests/src/main/java/com/example/pages/AdminPage.java
@@ -7,6 +7,10 @@ import static com.example.utils.GetUtility.getText;
 
 public class AdminPage extends BasePage {
 
+    private By logoutMenuButton = By.cssSelector("[data-testid='logout']");
+    private By tablePageMenuButton = By.cssSelector("[data-testid='nav-table']");
+    private By formPageMenuButton = By.cssSelector("[data-testid='nav-form']");
+
     private By adminPageHeader = By.xpath("//*[@id=\"root\"]/div/div/h2");
     private By adminRestrictedText = By.xpath("//*[@id='root']/div/div/div");
 
@@ -16,5 +20,23 @@ public class AdminPage extends BasePage {
 
     public String getAdminRestrictedText() {
         return getText(adminRestrictedText);
+    }
+
+    public TablePage clickTablePageButton() {
+        scroll(tablePageMenuButton);
+        click(tablePageMenuButton);
+        return new TablePage();
+    }
+
+    public FormPage clickFormPageButton() {
+        scroll(formPageMenuButton);
+        click(formPageMenuButton);
+        return new FormPage();
+    }
+
+    public LoginPage clickLogoutButton() {
+        scroll(logoutMenuButton);
+        click(logoutMenuButton);
+        return new LoginPage();
     }
 }
